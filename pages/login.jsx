51df6ffdb3d0b9fb548170f997c5f251ce6a42ae
VinceBruno <ambavincent1203@gmail.com>
2023-01-24 19:21:42 +0100

    UPDATE

--- pages/login.jsx
+++ pages/login.jsx
@@ -17,7 +17,7 @@ export default function login() {
 
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6"  method="POST">
               <div>
                 <label
                   htmlFor="email"
